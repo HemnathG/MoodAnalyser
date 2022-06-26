2df543dd7757749b74974533d5bfd74ac405c001
HemnathG <hemnathdavid@gmail.com>
2022-06-26 14:09:12 +0530

    Added Parameters to analyse the mood method

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -3,23 +3,23 @@ package com.bridgelabz;
 public class MoodAnalyser {
 	
 	String message;
-	
-	public MoodAnalyser() {
-		
-	}
-	
-	public MoodAnalyser(String message){
-		this.message = message;
-	}
 
-	public String analyseMood() {
-		
-		if(this.message.contains("Sad")) {
-			return "SAD";
-		}
-		else {
-			return "HAPPY";
-		}
-	}
+    public MoodAnalyser() {
+
+    }
+
+    public MoodAnalyser(String message){
+        this.message = message;
+    }
+
+    public String analyseMood() {
+
+        if(this.message.contains("Sad")) {
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
 
 }
